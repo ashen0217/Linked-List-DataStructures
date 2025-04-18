603b9c0478e9e816b2e09cd0c2dec6979e1f47db
Ashen Bamunusingha <ashen0217@gmail.com>
2025-04-18 20:45:43 +0530

    updre
    asaa

---
+++ linklist/src/numbersLinklist/linklist.java
@@ -0,0 +1,21 @@
+package numbersLinklist;
+
+public class linklist {
+
+    private linklist first;
+
+    public linklist(){
+        first = null;
+    }
+
+    //isEmpty() method
+    public boolean isEmpty(){
+        return (first == null);
+    }
+
+    //insert method
+    public void insertFirst(){
+
+    }
+
+}
